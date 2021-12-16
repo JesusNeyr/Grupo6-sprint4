aeb9b1bb265e6dcb6437342171796402dfe0cdd9
sosajuani <sosajuanignacio17@gmail.com>
2021-12-16 12:31:25 -0300

    Fin de los obligatorios del sprint, terminado en clase.Solo queda solucionar fallas de estilo

--- controller/controllerProduct.js
+++ controller/controllerProduct.js
@@ -10,6 +10,24 @@ const controllerProduct={
     create:(req,res)=>{
         res.render('admin/product/addProduct.ejs')
     },
+    crearAccion:(req,res)=>{
+        let body = req.body;
+        body.img = req.file.filename;
+        // const objt = {
+        //     id:0,
+        //     name:body.name,
+        //     precio:body.price,
+        //     descripcion:body.description,
+        //     img:req.file.filename,
+        //     peso:body.weight,
+        //     tamanio:body.size,
+        //     cat:body.cat,
+        //     offPorcen:body.discount,
+        //     cantidad:1000
+        // }
+        db.crear(body);
+        res.redirect("/")
+    },
     edit:(req,res)=>{
         let id=req.params.id;
         const buscar=db.buscar(id);
@@ -18,6 +36,9 @@ const controllerProduct={
     update:(req,res)=>{
         let id=req.params.id;
         const buscar=db.buscar(id);
+        let body = req.body;
+        body.id = buscar.id;
+        body.img = req.file.filename;
         // let objetoNew={
         //     "id":buscar.id,
         //     "name":req.body.name,
@@ -30,9 +51,16 @@ const controllerProduct={
         //     "offPorcen":req.body.offPorcen,
         //     "cantidad":req.body.cantidad,
         // }
+        db.actualizar(body);
+        res.redirect("/")
+    },
+    productDelete:(req,res)=>{
+        db.eliminar(req.params.id);
+        res.redirect('/');
     },
     productDetail:(req, res) =>{
-        res.render('pages/productDetail.ejs')
+        let articulo = db.buscar(req.params.id)
+        res.render('pages/productDetail.ejs',{articulo})
     }
 };
 module.exports=controllerProduct;
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const controllerProduct=require('../controller/controllerProduct');
+const upload = require('../middleware/multerMiddleware');
 
+//lista los productos
 router.get('/', controllerProduct.productos);
-router.get('/create', controllerProduct.create);
 
-router.get('/edit/:id', controllerProduct.edit);
-router.put('/edit/:id', controllerProduct.update);
+//vista del formulario crear
+
+router.get('/create', controllerProduct.create);
+//accion del formulario crear
+router.post('/', upload.single('img'), controllerProduct.crearAccion);
+
+router.get('/:id/edit', controllerProduct.edit);
+router.put('/:id/edit', upload.single('img'), controllerProduct.update);
 
 router.get('/:id', controllerProduct.productDetail);
+//accion eliminar
+router.post('/:id', controllerProduct.productDelete);
 module.exports=router;
